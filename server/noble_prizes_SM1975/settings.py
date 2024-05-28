b0068974cc6602c3ded1d040525dc408478373a0
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-05-28 18:04:01 +0800

    edit .gitignore and server projects/setting.py
    add cert/ and server/cert folder. add HTTPS SSL Certification for development setting at server/noble_prizes_SM1975

--- server/noble_prizes_SM1975/settings.py
+++ server/noble_prizes_SM1975/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# HTTPS SSL Certification for development setting
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
