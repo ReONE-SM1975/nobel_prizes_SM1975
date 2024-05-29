ab91460943b0100f54205992abc66b1849fc95d3
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-05-29 23:30:28 +0800

    add psycopg2 django-environ
    database should be another issue. \n wip: https for server

--- server/noble_prizes_SM1975/settings.py
+++ server/noble_prizes_SM1975/settings.py
@@ -11,22 +11,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool, True),
+    #DEBUG=(bool, False),
+)
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-joz$+xcc(0q0d+m2*q@d_$rk#(8f%gulz6syrp(_3wdh*+xo%t'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [x for x in env('HOSTNAMES').split(' ')]
 
 # Application definition
 
@@ -75,8 +80,9 @@ WSGI_APPLICATION = 'noble_prizes_SM1975.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
