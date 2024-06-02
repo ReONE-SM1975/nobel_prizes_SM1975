3f9219ec442c33ef14f2aac9c891849334d42aba
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-03 00:53:12 +0800

    settings.py change nobel

--- server/nobel_prizes_SM1975/settings.py
+++ server/nobel_prizes_SM1975/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'noble_prizes_SM1975.urls'
+ROOT_URLCONF = 'nobel_prizes_SM1975.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'noble_prizes_SM1975.wsgi.application'
+WSGI_APPLICATION = 'nobel_prizes_SM1975.wsgi.application'
 
 
 # Database
