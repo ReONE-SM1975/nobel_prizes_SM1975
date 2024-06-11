f54b51feb314e979b14f095d7fb3659db75e38ef
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-11 18:59:52 +0800

    admend missing dot present classes in models

--- server/myapp/admin.py
+++ server/myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Category, Country, City, AdminUser, Affiliation, Laureates, Winner
+from .models import Category, Country, City, AdminUser, Affiliation, Laureates, Winner
 
 
 # Register your models here.
