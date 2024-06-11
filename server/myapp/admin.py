6d3c437c2ce062929d2518e706b79fc988dc5596
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-11 18:57:14 +0800

    add admin.site.register({}models
    add all models in admin.py in myapp

--- server/myapp/admin.py
+++ server/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import Category, Country, City, AdminUser, Affiliation, Laureates, Winner
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(AdminUser)
+admin.site.register(Affiliation)
+admin.site.register(Laureates)
+admin.site.register(Winner)
