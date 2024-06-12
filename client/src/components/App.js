bcf46c379854b31c7dcd8db42533f42b46d4374f
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-12 22:21:32 +0800

    categroies client folder structures
    add components, styles, utils and pages folders for future extension
    
    moved App.js reportWebVitals.js into components/;
    
    moved index.css into styles
    
    created folder __test__ at components, pages, utils
    
    copied App.test.js into components/__test__, pages/__test__, utils/__test__
    
    installed react-router-dom
    
    prepare further structure design for nobel prizes page

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
