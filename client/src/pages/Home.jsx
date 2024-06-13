57571e14effc1a441a6635c5684b47485d385095
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-14 00:45:45 +0800

    build basic structure of home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 // import {useEffect, useState} from "react";
-import Header from "../components/Header"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 
 export default function Home(){
     return (
         <div>
-            <Header appTitle="The Nobel Prizes Database" />
+            <div className="Home">
+                <div className="Home__header">
+                    <Header appTitle="The Nobel Prizes Database" />
+                </div>
+                <div className="Home__Pannel">
+                    {"Under Constructions\n"}
+                    {"Under Constructions\n"}
+                    {"Under Constructions\n"}
+                    {"Under Constructions\n"}
+                    {"Under Constructions\n"}
+                </div>
+                <div className="Home__Footer">
+                    <Footer mainText={
+                        `Copyrights ${new Date().getFullYear()} ReONE-SM1975`
+                    } />
+                </div>
+
+            </div>
         </div>
     )
 }
