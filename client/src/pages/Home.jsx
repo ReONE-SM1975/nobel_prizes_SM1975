60b0116152d09fd23419246fc3e4da80be6473d2
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-28 23:13:33 +0800

    use then/catch instead async/await
    seems the original code setHello failed to collect any data from server - Problem: shouldn't async/await suppose to work too?

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,27 +6,32 @@ import axios from "axios"
 
 
 export default function Home(){
-    const [hello, setHello] = useState([{"text":"hello"}]);
+    const [hello, setHello] = useState([{"message":"should have 3 lines included this line"}]);
     useEffect(()=>{
-        const response = axios.create({baseURL:"http://localhost:8000"})
-        const header = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': false,
-        }
-        const fetchData = async () => {
-            try {
-                await response.get("/api/hello/", header);
-                console.log(response.data);
-                setHello( h =>
-                    h.concat(response.data)
-                )
+        // const response = axios.create({baseURL:"http://localhost:8000"})
+        // const header = {
+        //     'Accept': 'application/json',
+        //     'Content-Type': 'application/json',
+        //     'Access-Control-Allow-Origin': false,
+        // }
+        axios.get("http://localhost:8000/api/hello/").then(response => {
+            setHello(h => h.concat(response.data))
+        }).catch(error => console.error(error))
+        // const fetchData = async () => {
+        //     try {
+        //         await response.get("/api/hello/", header);
+        //         console.log(response.data);
+        //         setHello( 
+        //             h => h.concat(response.data)
+        //             //response.data
+        //         )
 
-            } catch (error){
-                console.error(error);
-            }
-        };
-        fetchData();
+        //     } catch (error){
+        //         console.error(error);
+        //     }
+        // };
+        //fetchData();
+
     }, []);
     return (
         <div>
@@ -40,7 +45,12 @@ export default function Home(){
                     {"Under Constructions\n"}<br/>
                     {"Under Constructions\n"}<br/>
                     {"Under Constructions\n"}<br/>
-                    { hello && `${hello.map(item=> JSON.stringify(item.message))}`}<br/>
+                    { hello && `${
+                        hello.map(item => 
+                            JSON.stringify(item.message))
+                    }\n`
+                        }
+                        <br/>
                 </div>
                 <div className="Home__Footer">
                     <Footer mainText={
--- server/myapp/views.py
+++ server/myapp/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+# from django.http import JsonResponse
 # from django.views.decorators.http import require_http_methods
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
 
 # @require_http_methods(['GET'])
+@api_view(['GET'])
 def hello_world(request):
-    data = list([{'message':'Hello, world!'},{'message':'Edge of Tomorrow World!'}])
-    return JsonResponse(
-        data, 
-        safe=False
+    data = [
+            {'message':'Hello, world!'},{'message':'Edge of Tomorrow World!'}
+            ]
+    #return JsonResponse(
+    return Response(
+        data 
+        #safe=False
         )
