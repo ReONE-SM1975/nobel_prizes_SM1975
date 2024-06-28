4e220f605c0fe098da44666d5806443839b20811
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-28 18:12:35 +0800

    add django-cors-headers as pip packages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -38,7 +38,7 @@ export default function Home(){
                     {"Under Constructions\n"}<br/>
                     {"Under Constructions\n"}<br/>
                     {"Under Constructions\n"}<br/>
-                    { hello && `${hello.map(item=> JSON.stringify(item))}`}<br/>
+                    { hello && `${hello.map(item=> JSON.stringify(item.message))}`}<br/>
                 </div>
                 <div className="Home__Footer">
                     <Footer mainText={
