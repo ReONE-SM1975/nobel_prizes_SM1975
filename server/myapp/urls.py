0e143ad992fdf091eec58ab2cd6bac6aaaaa508a
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-30 01:46:51 +0800

    urls.py add api's path
    wip showresult.jsx <- intent to display the result for json for later

--- server/myapp/urls.py
+++ server/myapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("hello/", views.hello_world, name="hello_world"),
-]
+    path("fullprizes/", views.get_allPrizes, name="all_prizes"),
+    path("fulllaureate/", views.get_allLaureate, name="all_laureate"),
+    path("fullcountriescode/", views.get_allCountries, name="all_countries_code")
+    ]
