aa9c03e5b88f39025a31a94d6c1fddbf618ecfc7
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-07-02 03:12:05 +0800

    import css to SideControlBar and ShowResults

--- client/src/components/ShowResults.jsx
+++ client/src/components/ShowResults.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 //import axios from "axios"
+import "../styles/ShowResults.css"
 
 export default function ShowResults(){
 
--- client/src/components/SideControlBar.jsx
+++ client/src/components/SideControlBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../styles/SideControlBar.css";
 
 export default function SideControlBar(){
 
