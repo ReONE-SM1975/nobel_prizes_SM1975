dde879435fa3254211f85681efff7d68852e0a75
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-07-04 04:40:44 +0800

    setSearchFound fetching resultTexts array

--- client/src/components/ShowResults.jsx
+++ client/src/components/ShowResults.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 //import axios from "axios"
 import "../styles/ShowResults.css";
 
 const resultTexts = (resultNum = 0) => {
+    resultNum = isNaN(parseInt(resultNum)) ? 0 : parseInt(resultNum);
     let single = "";
     if (resultNum !== 1){
         single = "s"
     }
-    return `Found ${resultNum} result${single}.`
+    return ["Found ", `${resultNum}`, ` result${single}.`];
 }
 
 export default function ShowResults(){
-
+    const [searchFound, setSearchFound] = useState([])
+    useEffect(()=>{
+        setSearchFound(resultTexts("foobar"))
+    },[]) 
     return (
         <div className="ShowResults">
             <div className="ShowResults__body">
                 <p className="ResultText">
-                    {`${resultTexts()}`}
+                    {`${searchFound[0]}`}
+                    <span className="ResultNumber">{`${searchFound[1]}`}</span>
+                    {`${searchFound[2]}`}
                 </p>
                 {"Under Constructions\n"}<br/>
                 {"Under Constructions\n"}<br/>
