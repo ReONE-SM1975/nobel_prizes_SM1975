7177d088163fa1137f88616d907b88c666e9f211
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-08-07 02:48:45 +0800

    wip: show full prize
    wip: prize.laureates as undefined

--- client/src/components/ShowResults.jsx
+++ client/src/components/ShowResults.jsx
@@ -19,9 +19,65 @@ export default function ShowResults({data=[]}){
         setDisplayData(data)
     },[data]) 
     
-    const prizeDisplay = () => {
-
+    const laureatesDisplay = (items) => {
+        return (
+            <>
+            { items.map((winner, idc)=>
+            {
+                return (
+                    <div key={idc+1000}>
+                            <ol>
+                                <li>
+                                {`[${winner.id}]`}
+                                {winner.surname ? `${winner.firstname} ${winner.surname}` : `${winner.firstname}`}<br />{`${winner.motivation}`}
+                                </li>
+                            </ol>
+                        </div>
+                )
+            })}
+            </>
+        )
     }
+
+    const prizeDisplay = (prizes) => {
+        return (
+            <div className="ResultBody">
+                    {
+                    prizes.map((prize,idx) => { /*
+                    <div key={idx}>
+                    <h3>{`${prize.year} : ${prize.cateogry}`}</h3>
+                    { prize.overallmotivation && <h3>{`${prize.overallmotivation}`}</h3>}
+                    <h2>{`Laureates:`}</h2>
+                    <ol>
+                    <h2>{prize.laureates.map((winner, inc) => {
+                        let fullname = winner.surname ? `${winner.firstname} ${winner.surname}` : `${winner.firstname}`;
+                            <li>{`${inc+1}:[${winner.id}] ${fullname}}`}<br />{`${winner.motivation}`}</li>
+                    })}</h2>
+                    </ol>
+                    </div>
+                   */ 
+                return (
+                    <div key={idx}>
+                        <h2>
+                            {`[${idx + 1}]: ${prize.year} : ${prize.category}`}
+                        </h2>
+                        <h2>
+                            {prize.overallMotivation && `${prize.overallMotivation}`}
+                        </h2>
+                        <h2>{`Laureates:`}</h2>
+                        {/* {prize.laureates} */}
+                    {/* {`${(laureatesDisplay(prize.laureates))}`} */}
+            </div>
+                )
+                }
+                    )
+                }
+            </div>
+            )
+    }
+    
+
+
 
     return (
         <div className="ShowResults">
@@ -32,7 +88,8 @@ export default function ShowResults({data=[]}){
                     {`${searchFound[2]}`}
                 </p>
                 <div>
-                    { displayData && displayData.map((ele) => { return <p>{`${ele.year}:${ele.category}\n${ele.laureates}`}</p>}) } 
+                    { /*displayData && displayData.map((ele) => { return <p>{`${ele.year}:${ele.category}\n${ele.laureates}`}</p>}) */} 
+                    { displayData && prizeDisplay(displayData)}
                 </div>
                 
                 
