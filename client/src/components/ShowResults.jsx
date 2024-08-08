068161b752374a343324e44308973cfef8de2f89
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-08-08 17:22:53 +0800

    feat: default load list of all prizes

--- client/src/components/ShowResults.jsx
+++ client/src/components/ShowResults.jsx
@@ -12,61 +12,46 @@ const resultTexts = (resultNum = 0) => {
 }
 
 export default function ShowResults({data=[]}){
-    const [displayData, setDisplayData] = useState(data)
-    const [searchFound, setSearchFound] = useState(0)
-    //const [totalLaureates, setTotalLaureates] = useState(0)
+    const [displayData, setDisplayData] = useState(data);
+    const [searchFound, setSearchFound] = useState(0);
     
     useEffect(()=>{
-        // setSearchFound(resultTexts(data.length))
-        
-            setDisplayData(data)
-            setSearchFound(resultTexts(data.length))
-        
+        setDisplayData(data)
+        setSearchFound(resultTexts(data.length))
+    },[data]); 
     
-    },[data]) 
-    
-    const laureatesDisplay = (items) => {
-        console.log("laureatesDisplay:",items)
+    function laureatesDisplay (items) {
         return (
             <div className="laureatesHead">
-            <ol className="laureatesBody">
-            { items.map((winner, idc)=>
-            {
-                //setTotalLaureates(totalLaureates+1)
-                return (
-                    <li key={winner.firstname}>
-                    {`[${winner.id}] : `}{'WINNER : '}{winner.surname ? `${winner.firstname} ${winner.surname}` : `${winner.firstname} `}{`${winner.motivation}`}
-                    </li>            
-                )
-            }) }
-            </ol>
+                <div className="laureatesBody">
+                    { items && items.map(({id, surname, firstname, motivation, share}, idc) => {
+                        return (
+                            <div key={`${firstname}${id}${idc}`}>
+                                <div>{idc + 1} / {share} {id} : {firstname} {surname ? surname : ""}</div>
+                                <div>{motivation}</div>
+                            </div>            
+                        );
+                    }) }
+                </div>
             </div>
-        )
+        );
     }
 
-    const prizeDisplay = (prizes) => {
+    function prizeDisplay (prizes) {
         return (
             <div className="ResultBody">
-                    {
-                    prizes && prizes.map((prize,idx) => { 
+                { prizes && prizes.map((prize,idx) => { 
                 return (
                     <div key={idx}>
-                        <h3>
-                            {`[${idx + 1}]: ${prize.year} : ${prize.category}`}
-                        </h3>
-                        <h3>
-                            {prize.overallMotivation && `${prize.overallMotivation}`}
-                        </h3>
-                        <h3>{`Laureates:`}</h3>
-                        {/* {prize.laureates} */}
-                    {prize.laureates && `${(laureatesDisplay(prize.laureates))}`}
-            </div>
-                )
-                }
-                    )
+                        <h3>{`[${idx + 1}]: ${prize.year} : ${prize.category}`}</h3>
+                        <h3>{prize.overallMotivation && `${prize.overallMotivation}`}</h3>
+                        <div>{`Laureates:`}</div>
+                        <div>{prize.laureates && laureatesDisplay(prize.laureates)}</div>
+                    </div>
+                );})
                 }
             </div>
-            )
+        );
     }
     
 
@@ -75,13 +60,12 @@ export default function ShowResults({data=[]}){
     return (
         <div className="ShowResults">
             <div className="ShowResults__body">
-                <p className="ResultHeader">
+                <div className="ResultHeader">
                     {`${searchFound[0]}`}
                     <span className="ResultNumber">{`${searchFound[1]}`}</span>
                     {`${searchFound[2]}`}
-                </p>
+                </div>
                 <div>
-                    { /*displayData && displayData.map((ele) => { return <p>{`${ele.year}:${ele.category}\n${ele.laureates}`}</p>}) */} 
                     { displayData && prizeDisplay(displayData)}
                 </div>
                 
