b7a22d7cf5d4b6889b86f2945370a4f90879fac0
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-08-14 18:32:45 +0800

    feat: show greeting message at header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-
+import TimeOfDay from './TimeOfDay'
 import "../styles/Header.css"
 
-export default function Header({appTitle, welcomeText, randomWinner, timeOfDay}){
+export default function Header({ appTitle, welcomeText, randomWinner, timeOfDay }) {
     return (
         <div className="Header">
             <header className="Header__header">
-                { appTitle && <h1 className="header__title">{ appTitle }</h1>}
-                { welcomeText && <h2 className="header__welcomeText">{ welcomeText}</h2>}
-                { randomWinner && <b><div className="header__randomWinner">{ randomWinner}</div></b>}
-                { timeOfDay && <h3 className="header__timeOfDay"> {timeOfDay} </h3>}
+                {appTitle && <h1 className="header__title">{appTitle}</h1>}
+                {welcomeText && <h2 className="header__welcomeText">{welcomeText}</h2>}
+                {randomWinner && <b><div className="header__randomWinner">{randomWinner}</div></b>}
+                {timeOfDay && <TimeOfDay className="header__timeOfDay" />}
 
             </header>
+
         </div>
     )
 }
--- client/src/components/TimeOfDay.jsx
+++ client/src/components/TimeOfDay.jsx
@@ -1,74 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const t = new Date();
-const greetings = [
-    "How is it going?",
-    "How are you today?",
-    "Greetings.",
-    "Good day!",
-    "Welcome!",
-    "Hi.",
-    "Hello.",
-    "At your service.",
-];
-const period = [
-    "Good night!",
-    "Good morning!",
-    "Good afternoon",
-    "Good evening!",
-];
-const Months = [
-    "January", 
-    "February", 
-    "March", 
-    "April", 
-    "May", 
-    "June", 
-    "July", 
-    "August", 
-    "September", 
-    "October", 
-    "November", 
-    "December"
-];
+export default function TimeOfDay(className) {
 
-function supth (dd) {
-    const special = [1,2,3,21,22,23,31]
-    if (special.includes(dd)){
-        if(dd === 1 || dd === 21 || dd === 31){
-            return "st";
-        } else if (dd === 2 || dd === 22){
-            return "nd";
-        } else if (dd === 3 || dd === 23){
-            return "rd";
+    const greetings = [
+        "How is it going?",
+        "How are you today?",
+        "Greetings.",
+        "Good day!",
+        "Welcome!",
+        "Hi.",
+        "Hello.",
+        "At your service.",
+    ];
+    let t = new Date();
+    const [date, setTheDate] = useState(0)
+    const [hour, setTheHours] = useState(0)
+    const [min, setTheMinutes] = useState(0)
+    const [month, setTheMonth] = useState(0)
+    const [year, setTheYear] = useState(0)
+    const [randomGreetingNum, setRandomGreetingNum] = useState(0)
+    const [greetingMessage, setGreetingMessage] = useState("")
+
+    useEffect(() => {
+        setTheDate(t.getDate());
+        setTheHours(t.getHours());
+        setTheMinutes(t.getMinutes());
+        setTheMonth(t.getMonth());
+        setTheYear(t.getFullYear());
+        console.log(`hours: `, hour, ` min: `, min, ` Date: `, date);
+    }, [min]);
+
+    useEffect(() => {
+        setRandomGreetingNum(Math.floor(Math.random() * (greetings.length)));
+        setGreetingMessage(greetings[randomGreetingNum])
+    }, []);
+
+    const period = [
+        "Good night!",
+        "Good morning!",
+        "Good afternoon!",
+        "Good evening!",
+    ];
+
+    const Months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    function supth(dd) {
+        const special = [1, 2, 3, 21, 22, 23, 31]
+        if (special.includes(dd)) {
+            if (dd === 1 || dd === 21 || dd === 31) {
+                return "st";
+            } else if (dd === 2 || dd === 22) {
+                return "nd";
+            } else if (dd === 3 || dd === 23) {
+                return "rd";
+            }
+        } else {
+            return "th";
         }
-    } else {
-        return "th";
     }
-}
 
-function getTimeOfDay(hh,mm){
-    if (hh < 4 || hh >= 21){
-        return period[0];
-    } else if (hh >= 4 && hh < 12){
-        return period[1];
-    } else if (hh >= 12 && hh < 17){
-        return period[2];
-    } else if (hh >= 17 && hh < 20){
-        if (mm < 30){
+    function getTimeOfDay(hh, mm) {
+        if (hh < 4 || hh >= 21) {
+            return period[0];
+        } else if (hh >= 4 && hh < 12) {
+            return period[1];
+        } else if (hh >= 12 && hh < 17) {
             return period[2];
-        } else if (mm >= 30){
+        } else if (hh >= 17 && hh < 20) {
             return period[3];
+        } else if (hh === 20) {
+            if (mm < 30) {
+                return period[3];
+            } else if (mm >= 30) {
+                return period[0];
+            }
+        } else {
+            return "error: Check getTimeOfDay Code"
         }
-    } else {
-        return "error: Check getTimeOfDay Code"
     }
-}
-
-export default function TimeOfDay(){
     return (
         <>
-        <div></div>
+            <div className={`TimeOfDay ${className}`}>
+                {`${getTimeOfDay(hour, min)} ${greetingMessage} Today is ${date}`}<sup>{`${supth(date)}`}</sup>{` ${Months[month]} ${year}.`}</div>
         </>
     )
 }
