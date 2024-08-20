fc1a1675c0d90cba6e094c3b4a8a1b527f6f0fbc
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-08-20 16:43:07 +0800

    remove unused codes at Random Winner.jsx

--- client/src/components/RandomWinner.jsx
+++ client/src/components/RandomWinner.jsx
@@ -1,47 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// const ACTION = {
-//     WINNER : 'winner',
-//     PAYLOAD : 'payload',
-//     RETURNDATA: 'returnData',
-//     RANDOMLAUREATES: 'randomLaureates'
-// }
-
-
-// function reducer (state, action) {
-
-
-//     switch (action.type){
-//         default:
-//             return state;
-//         case ACTION.RETURNDATA:
-//             const fetch = "http://localhost:8000/api/randomwinner/"
-//             const response = await axios.post(fetch, payload)
-//             return { returnData : response }
-//     }
-// }
-
-
-
 export default function RandomWinner() {
-    //const [prizes, setPrizes] = useState([])
     const [winner, setWinner] = useState({});
     const [payload, setPayload] = useState({});
     const [returnData, setReturnData] = useState({});
-    //const [randomLaureates, setRandomLaureates] = useState(0);
-
-    /*   const [state, dispatch] = useReducer(reducer, {
-           winner: {},
-           payload: {},
-           returnData: {},
-           randomLaureates: 0,
-           error: null,
-       }) */
-
-    /*    function handleUpdates(){
-            dispatch({type: ACTION.RETURNDATA})
-        } */
 
     function getRandom(min, max) {
         if (min > max) {
@@ -65,7 +28,6 @@ export default function RandomWinner() {
             "year": `${getRandom(1901, new Date().getFullYear() - 1)}`,
             "category": `${categories[getRandom(0, categories.length - 1)]}`,
         })
-
     }, []);
 
     useEffect(() => {
@@ -82,7 +44,6 @@ export default function RandomWinner() {
     useEffect(() => {
         setWinner((prev) => {
             if (returnData.prizes) {
-                //const prize = returnData.prizes[0]
                 const { laureates } = returnData.prizes[0]
                 const randomCandidate = getRandom(0, laureates.length - 1);
                 const winnerDetails = laureates[randomCandidate]
@@ -110,10 +71,10 @@ export default function RandomWinner() {
 
     return (
         <>
-            <h4>
+            <h6>
                 {winner && `${winner["motivation"]} `}
                 {<span>{winner && `${winner["winner"]}`}</span>}
-                {`(${payload["year"]}) in ${payload["category"]}`}</h4>
+                {`(${payload["year"]}) in ${payload["category"]}`}</h6>
 
         </>
 
