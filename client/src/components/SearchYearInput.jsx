03aacd2362b932ee38c352c033ba5278cd50f33a
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-08-28 02:29:22 +0800

    wip: searchYearInput basic setup-not yet tested

--- client/src/components/SearchYearInput.jsx
+++ client/src/components/SearchYearInput.jsx
@@ -1,26 +1,95 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Input from './Input';
+import '../styles/SearchYearInput.css';
 
-export default function SearchYearInput({ className, name, id, patten, placeholder, maxLength, onChange }) {
-    const [submitAttempt, setSubmitAttempt] = useState(false)
+export default function SearchYearInput({ className, name, id, onChange }) {
+    //const [typing, setTyping] = useState(false);
+    const [incorrectTyping, setIncorrectTyping] = useState(false);
     const [searchYear, setSearchYear] = useState("");
     const [result, setResult] = useState({
         "year": "",
         "yearTo": "",
     })
+
+    function writeSearchYear(init, year, key){
+        if(Number(year) < 1901){
+            return { ...init, [key]: 1901}
+        } else if (Number(year) > new Date().getFullYear()) {
+            return { ...init, [key]: new Date().getFullYear()}
+        } else {
+            return { ...init, [key]: year}
+        }
+    }
+
     function handleOnChange(e) {
         setSearchYear(e.target.value)
         if (searchYear) {
-
+            if (searchYear.length < 4){
+                setIncorrectTyping(true)
+            } else if (searchYear.length === 4){
+                if (isNaN(Number(searchYear))){
+                    setIncorrectTyping(true)
+                } else {
+                    setResult((prev) => {
+                        // if(Number(searchYear) < 1901){
+                        //     return { ...prev, "year": 1901}
+                        // } else if (Number(searchYear) > new Date().getFullYear()) {
+                        //     return { ...prev, "year": new Date().getFullYear()}
+                        // } else {
+                        //     return { ...prev, "year": searchYear}
+                        // }
+                        writeSearchYear(prev, searchYear, "year");
+                        setIncorrectTyping(false)
+                    })
+                }
+            } else if (searchYear.length < 9){
+                setIncorrectTyping(true)
+            } else if (searchYear.length === 9){
+                if (searchYear[4] !== "-"){
+                    setIncorrectTyping(true)
+                } else {
+                    if (isNaN(Number(searchYear.slice(0,4))) || isNaN(Number(searchYear.slice(5,9)))){
+                        setIncorrectTyping(true)
+                        setResult({"year":"", "yearTo":""})
+                    } else {
+                        setIncorrectTyping(false)
+                        setResult(() => {
+                            let start = Number(searchYear.slice(0,4))
+                            let end = Number(searchYear.slice(5,9))
+                            if (start > end ){
+                                [start, end] = [end, start]
+                            } else if (start === end){
+                                end = ""
+                            }
+                            return {
+                                "year":start,
+                                "yearTo":end
+                            }
+                        })
+                    }
+                }
+            }
+            
+        } else {
+            setIncorrectTyping(false)
+            setResult({"year":"","yearTo":""})
         }
-        onChange(e.target.value)
+        onChange(result)
 
     }
 
     return (
         <>
-            <Input className="SearchTextBar" name="year" id="year" pattern={`[0-9]{4}-[0-9]{4}`} placeholder={'YYYY or YYYY-YYYY'} maxLength={"9"} onChange={handleOnChange} />
-            {submitAttempt && searchYear && <p className="SearchYear__Warning">{`Warning: search year required years in the form of YYYY or YYYY-YYYY format`}</p>}
+            <Input 
+            className={className}
+            name={name} 
+            id={id} 
+            pattern={`[0-9]{4}-[0-9]{4}`} 
+            placeholder={'YYYY or YYYY-YYYY'} 
+            maxLength={"9"} 
+            onChange={handleOnChange} 
+            /><br />
+            {searchYear && incorrectTyping && <p className="SearchYear__Warning">{`Hint: search year required years in the form of YYYY or YYYY-YYYY format`}</p>}
         </>
     )
 }
--- client/src/components/SideControlBar.jsx
+++ client/src/components/SideControlBar.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Input from "../components/Input";
 import Button from "../components/Button";
 import DropDownDataList from "./DropDownDataList";
+import SearchYearInput from "./SearchYearInput";
 
 
 import "../styles/SideControlBar.css";
@@ -30,7 +31,7 @@ export default function SideControlBar() {
         "idTo": "",
         "keywords": "",
     })
-    const [searchYear, setSearchYear] = useState("");
+    const [searchYear, setSearchYear] = useState({});
     const [searchYearTo, setSearchYearTo] = useState("");
     const [searchCat, setSearchCat] = useState("");
     const [searchFirstName, setSearchFirstName] = useState("");
@@ -45,72 +46,76 @@ export default function SideControlBar() {
 
     const handleSearchYear = (e) => {
         e.preventDefault();
-        setSearchYear(e.target.value);
+        // setSearchYear(e.target.value);
     }
 
     const handleSearchCat = (e) => {
         e.preventDefault();
-        setSearchCat(e.target.value);
+        // setSearchCat(e.target.value);
     }
 
     const handleSearchFirstName = (e) => {
         e.preventDefault();
-        setSearchFirstName(e.target.value);
+        // setSearchFirstName(e.target.value);
     }
 
     const handleSearchSurname = (e) => {
         e.preventDefault();
-        setSearchSurname(e.target.value);
+        // setSearchSurname(e.target.value);
     }
 
     const handleSearchCountry = (e) => {
         e.preventDefault();
-        setSearchCountry(e.target.value);
+        // setSearchCountry(e.target.value);
     }
 
     const handleSearchCity = (e) => {
-        e.preventDefault();
+        // e.preventDefault();
     }
 
     const handleSubmitSearch = (e) => {
         e.preventDefault();
-        if (searchYear) {
-            if (searchYear.length < 4) {
-                setAttempttedSubmit(true)
-            } else if (searchYear.length === 4) {
-                if (isNaN(searchYear)) {
-                    setAttempttedSubmit(true)
-                } else {
-                    /* 
-                    send search year request to nobel prize
-                    */
-                    setAttempttedSubmit(false)
-                }
-            } else if (searchYear.length < 9) {
-                setAttempttedSubmit(true)
-            } else if (searchYear.length === 9) {
-                if (searchYear[4] !== '-') {
-                    setAttempttedSubmit(true);
-                } else {
-                    setSearchYearContainer(searchYear.split("-"))
-                    /**
-                     * searchYearContainer[0] will be the year where start of range be
-                     */
-                    setAttempttedSubmit(false)
-                    if (isNaN(searchYearContainer[0]) || isNaN(searchYearContainer[1])) {
-                        setAttempttedSubmit(true);
-                    } else {
-                        setSearchYearTo(searchYearContainer[1])
-                        /**
-                         * send search year and yearto request to nobel prize 
-                         */
-                        setAttempttedSubmit(false)
+        console.log(searchData)
+        // if (searchYear) {
+        //     if (searchYear.length < 4) {
+        //         setAttempttedSubmit(true)
+        //     } else if (searchYear.length === 4) {
+        //         if (isNaN(searchYear)) {
+        //             setAttempttedSubmit(true)
+        //         } else {
+        //             /* 
+        //             send search year request to nobel prize
+        //             */
+        //             setAttempttedSubmit(false)
+        //         }
+        //     } else if (searchYear.length < 9) {
+        //         setAttempttedSubmit(true)
+        //     } else if (searchYear.length === 9) {
+        //         if (searchYear[4] !== '-') {
+        //             setAttempttedSubmit(true);
+        //         } else {
+        //             setSearchYearContainer(searchYear.split("-"))
+        //             /**
+        //              * searchYearContainer[0] will be the year where start of range be
+        //              */
+        //             setAttempttedSubmit(false)
+        //             if (isNaN(searchYearContainer[0]) || isNaN(searchYearContainer[1])) {
+        //                 setAttempttedSubmit(true);
+        //             } else {
+        //                 setSearchYearTo(searchYearContainer[1])
+        //                 /**
+        //                  * send search year and yearto request to nobel prize 
+        //                  */
+        //                 setAttempttedSubmit(false)
 
-                    }
-                }
-            }
+        //             }
+        //         }
+        //     }
 
-        }
+        // }
+    }
+    function handleYearOnChange(obj){
+        setSearchData((prev)=>({...prev,obj}))
     }
 
     return (
@@ -119,8 +124,8 @@ export default function SideControlBar() {
             <div className="SideControlBar__body">
                 <form htmlFor="searchTerms">
                     <label className="SearchLabel" htmlFor="year">Year:</label>
-                    <Input className="SearchTextBar" name="year" id="year" pattern={`[0-9]{4}-[0-9]{4}`} placeholder={'YYYY or YYYY-YYYY'} maxLength={"9"} onChange={handleSearchYear} /><br />
-                    {attempttedSubmit && searchYear && <p className="SearchYear__Warning">{`Warning: search year required years in the form of YYYY or YYYY-YYYY format`}</p>}
+                    <SearchYearInput className="SearchTextBar" name="year" id="year" onChange={handleYearOnChange} /><br />
+                    {/* {attempttedSubmit && searchYear && <p className="SearchYear__Warning">{`Warning: search year required years in the form of YYYY or YYYY-YYYY format`}</p>} */}
                     <label className="SearchLabel" htmlFor="search_cat">Categories:</label>
                     <DropDownDataList className="SearchTextBar" list="searchCat" options={categories} name="search_cat" id="search_cat" placeholder="doubleclick avaliable" /><br />
                     <label>Firstname:</label>
