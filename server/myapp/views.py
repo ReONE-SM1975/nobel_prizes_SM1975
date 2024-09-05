642806ac33bec1d87f169014b3610752eb88a443
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-09-06 05:23:34 +0800

    feat: app server get official data via query

--- server/myapp/views.py
+++ server/myapp/views.py
@@ -66,9 +66,13 @@ def searchofficial(request):
         query_prize = ["year", "yearTo", "category"]
         query_laureates = ["bornCity","diedCity","bornCountry","diedCountry", "affiliation"]
         query_others = ["firstname", "surname", "keyword"]
-        
-        my_dict = {
-
-        }
-        return Response({"payload" : str(payload),"keys": str([{str(i):str(payload[i])} for i in payload if i in query_prize] + [{str(j):str(payload[j])} for j in payload if j in query_laureates] + [k for k in payload if k in query_others])})
+        my_dict = {}
+        text = []
+        char = "&"
+        for key in payload:
+            if key in query_prize:
+                text.append(f"{key}={payload[key]}")
+        response = requests.get(f"https://api.nobelprize.org/v1/prize.json?{char.join(text)}")
+        return Response(response.json())
+        #return Response({"payload" : str(payload),"keys": str([{str(i):str(payload[i])} for i in payload if i in query_prize] + [{str(j):str(payload[j])} for j in payload if j in query_laureates] + [k for k in payload if k in query_others])})
     return Response({"message": str(f"payload empty: {message[0]}")})
