16827250ece29a0fb327ae5cbd5f8a0d7d4c09d8
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-09-09 02:58:14 +0800

    wip server brain storm for searchofficial

--- server/myapp/views.py
+++ server/myapp/views.py
@@ -64,19 +64,50 @@ def searchofficial(request):
     payload = request.data
     if (payload):
         query_prize = ["year", "yearTo", "category"]
-        query_laureates = ["city","country","affiliation"]
-        query_others = ["firstname", "surname", "keyword", "id", "idTo"]
-        my_dict = {}
-        text = []
+        query_laureates = ["city","country","affiliation", "keyword"]
+        query_others = ["firstname", "surname", "id", "idTo"]
+        result = {
+            "prizes":[]
+        }
+        ids_group = []
+        prizesquery = []
+        laureatesquery = []
+        othersquery = []
         char = "&"
         url = "https://api.nobelprize.org/v1/"
-        prizejson = "prize.json"
+        prizesjson = "prize.json"
         laureatesjson = "laureate.json"
-        countryjson = "country.json"
+        # countryjson = "country.json"
         for key in payload:
-            if key in query_prize:
-                text.append(f"{key}={payload[key]}")
-        response = requests.get(f"https://api.nobelprize.org/v1/prize.json?{char.join(text)}")
-        return Response(response.json())
+            
+            prizesquery.append(f"{key}={payload[key]}")
+            # elif key in query_laureates:
+            #     laureatesquery.append(f"{key}={payload[key]}")
+            # elif key in query_others:
+            #     othersquery.append(f"{key}={payload[key]}")
+        # if prizesquery and not laureatesquery and not othersquery:
+        response = requests.get(f"{url}{prizesjson}?{char.join(prizesquery)}")
+        data = response.json()
+        return Response(data)
+        # elif(prizesquery and laureatesquery and not othersquery):
+        #     prizes = data.prizes
+        #     for prize in prizes:
+        #         for laureates in prize.laureates:
+        #             if laureates.id:
+        #                 ids_group.append(laureates.id)
+        #     for id in ids_group:
+        #         return
+        # elif laureatesquery and othersquery:
+        #     return
+        
+            
+                
+        # else:
+        #     response = requests.get(f"{url}{laureatesjson}")
+
         #return Response({"payload" : str(payload),"keys": str([{str(i):str(payload[i])} for i in payload if i in query_prize] + [{str(j):str(payload[j])} for j in payload if j in query_laureates] + [k for k in payload if k in query_others])})
-    return Response({"message": str(f"payload empty: {message[0]}")})
+    return Response({
+                    "message" : "payload has error or empty",
+                    "payload" : str(request.data),
+                    "request" : str(request),
+                    })
